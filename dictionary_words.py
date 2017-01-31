48134a04b7cd1f3ad3da4a5302d2a0524cf5875c
Ross Justin <justrostin@gmail.com>
2017-01-31 15:12:30 -0800

    finished dictionary_words.py

---
+++ dictionary_words.py
@@ -0,0 +1,20 @@
+import random
+
+
+def random_sentence(dictionary_words, sentence_length):
+    dictionary_length = len(dictionary_words)
+    random_string = ""
+    for i in range(0, sentence_length):
+        word_index = random.randint(0, dictionary_length)
+        random_string += dictionary_words[word_index] + " "
+    return random_string
+
+
+if __name__ == '__main__':
+    print("How many words?")
+    sentence_length = int(raw_input())
+    with open('/usr/share/dict/words') as f:
+        dictionary_words = f.readlines()
+        dictionary_words = [x.strip() for x in dictionary_words]
+
+    print(random_sentence(dictionary_words, sentence_length))
---
+++ template.py
@@ -0,0 +1,9 @@
+import random
+
+
+def function():
+    return "test"
+
+
+if __name__ == '__main__':
+    print(function())
