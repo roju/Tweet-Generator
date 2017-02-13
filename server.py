c894539e1e07aaec4a2ceb0a45506b05131c3f5c
Ross Justin <justrostin@gmail.com>
2017-02-13 12:17:41 -0800

    deleted unneeded files

--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import sample
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    text = open('huckfinn.txt', 'r')
-    return sample.random_sentence(sample.histogram(text), 10)
